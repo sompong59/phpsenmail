b772ecdc73a52f03731923dbee7a4772fbf5e721
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-01 20:44:13 +0700

    Update index.php

--- phpmail-contact-form-main/index.php
+++ phpmail-contact-form-main/index.php
@@ -4,8 +4,7 @@
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>PHP Contact Form</title>
-	<link rel="stylesheet" type="text/css" 
-	      href="style.css">
+	<link rel="stylesheet" href="style.css">
           
 </head>
 <style>
@@ -27,7 +26,7 @@
     
 </style>
 <body>
-  <div class="form-container">
+    <div class="container">
   	<h2>ປ້ອນຂໍ້ມູນເພື່ອຮ້ອງຂໍການແປ</h2>
   	<?php if (isset($_GET['error'])) { ?>
   		<p class="error">
@@ -44,9 +43,9 @@
 
   	<!-- <form action="contact.php" method="POST"> -->
       <form action="contact.php" method="POST" enctype="multipart/form-data">
-	  <div>
-<label">ເລືອກພາສາຕົ້ນສະບັບ</label>
-        <div>
+        <div class="language-group">
+            <div class="form-group">
+<label>ເລືອກພາສາຕົ້ນສະບັບ</label>
             <select type="text" name="originalLanguage">
                 <option value="ພາສາລາວ ">ພາສາລາວ </option>
                 <option value="ພາສາອັງກິດ ">ພາສາອັງກິດ </option>
@@ -58,10 +57,9 @@
                 <option value="ອື່່ນ ໆ">ອື່ນ ໆ</option>
             </select>
         </div>
-</div>
-
-<label">ເລືອກພາສາເປົ້າໝາຍ</label>
-        <div>
+      
+        <div class="form-group">
+<label>ເລືອກພາສາເປົ້າໝາຍ</label>
             <select type="text" name="targetLanguage">
 			<option value="ພາສາລາວ ">ພາສາລາວ </option>
                 <option value="ພາສາອັງກິດ ">ພາສາອັງກິດ </option>
@@ -73,37 +71,46 @@
                 <option value="ອື່່ນ ໆ">ອື່ນ ໆ</option>
             </select>
         </div>
-</div>
+        </div>
 
-<div>
-<label> ແນບໄຟລ໌ຂອງທ່ານ (ສູງສຸດ 5 ໄຟລ໌) </label>
-<input type="file" name="form_fields[]" multiple="multiple" data-maxsize="10" data-maxsize-message="This file exceeds the maximum allowed size.">    </div>
-	<div>
+        <div class="form-group">
+            <label> ແນບໄຟລ໌ຂອງທ່ານ (ສູງສຸດ 5 ໄຟລ໌) </label>
+        <div class="file-upload">
+            <input type="file" name="form_fields[]" multiple="multiple" data-maxsize="10" data-maxsize-message="This file exceeds the maximum allowed size.">   
+        </div>
+        </div>
+        
+        <div class="form-group">
             <label>ປ້ອນຂໍ້ມູນຊື່ຂອງທ່ານ </label>
             <input type="text" name="name"
                 placeholder="ຊື່ຂອງຂອງ"
                 required>
         </div>
+        <div class="language-group">
+        <div class="form-group">
 		<label> ທີ່ຢູ່ອີເມວຂອງທ່ານ	</label>
   		<input type="email" name="email" 
   		       placeholder="ທີ່ຢູອີເມວ" 
   		       required>
-                 <!-- <label> ເບີໂທຂອງທ່ານ	</label> -->
-  		<!-- <input type="text" name="phoneNamber" 
-  		       placeholder="Your Email" 
-  		       required>
-				 <div> -->
+            </div> 
+            <div class="form-group">
             <label> ເບີໂທຂອງທ່ານ </label>
             <input size="1" type="tel" name="phoneNamber" placeholder="ເບີໂທຂອງທ່ານ" pattern="[0-9()#&amp;+*-=.]+" title="Only numbers and phone characters (#, -, *, etc) are accepted.">
         </div> 
+    </div>
+    <div class="form-group">
   		<input type="text" name="subject" 
   		       placeholder="Subject" 
   		       required>
+            </div>
+            <div class="form-group">
+                <label for="message">ຂໍ້ຄວາມ</label>
   		<textarea name="text" 
   		          rows="5"
-  		          placeholder="Your Message"></textarea>
+  		          placeholder="ກະລຸນາແຈ້ງຄວາມຕ້ອງການຂອງທ່ານ ເຊັ່ນ ຈຸດປະສົງຂອງການໃຊ້ຄໍາແປ ຯລຯ"></textarea>
+                </div>
   		<button type="submit">ສົ່ງຂໍ້ມູນ</button>
-          </div>
+          
   	</form>
       <div>
  
